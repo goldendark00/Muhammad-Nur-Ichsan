a393e740eb97eaad279c70aef9d73275e1f56954
goldendark00 <43660882+goldendark00@users.noreply.github.com>
2018-10-31 22:39:03 +0800

    Create MainStack.java

---
+++ MainStack.java
@@ -0,0 +1,10 @@
+
+public class MainStack {
+
+	public static void main(String[] args) {
+		  Stack<Integer> s = new Stack<Integer> ();
+     s.push (17);
+     int i = s.pop ();
+     	System.out.format ("%4d%n", i);
+    }
+}
