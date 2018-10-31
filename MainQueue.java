c2dc505acc256716f8a5a0d371af22fd33dcfe7f
goldendark00 <43660882+goldendark00@users.noreply.github.com>
2018-10-31 22:40:09 +0800

    Create MainQueue.java

---
+++ MainQueue.java
@@ -0,0 +1,10 @@
+
+public class MainQueue {
+
+	public static void main(String[] args) {
+		Queue<Integer> s = new Queue<Integer> ();
+     s.insert (17);
+     int i = s.get ();
+     	System.out.format ("%4d%n", i);
+    }
+}
